ac4f5cba1dcd3dfedc83822696cb0af4933ac168
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2024-10-23 11:25:24 +0000

    feat: :sparkles: novos metodos

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
 
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongo', 27017, username=os.getenv('ME_CONFIG_MONGODB_ADMINUSERNAME'), password=os.getenv('ME_CONFIG_MONGODB_ADMINPASSWORD'))
 
 db = client.flask_db
 todos = db.todos
@@ -20,3 +22,8 @@ def index():
 
     all_todos = todos.find()
     return render_template('index.html', todos=all_todos)
+
+@app.post('/<id>/delete/')
+def delete(id):
+    todos.delete_one({"_id": ObjectId(id)})
+    return redirect(url_for('index'))
